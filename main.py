fe83afa0a13a5c89abcd350ae88cadd0eb205ad0
carloosfalco <carloosfalco@gmail.com>
2025-06-17 16:25:19 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from rutas import planificador_rutas
+from orden_carga_generator import generar_instrucciones_ruta
+
+
+def main():
+    st.set_page_config(page_title="Virosque TMS", page_icon="🚛", layout="wide")
+
+    st.sidebar.title("📂 Menú")
+    seleccion = st.sidebar.radio("Selecciona una opción", [
+        "Planificador de rutas",
+        "Orden de carga",
+
+    ])
+
+    if seleccion == "Planificador de rutas":
+        planificador_rutas()
+    elif seleccion == "Orden de carga":
+        generar_instrucciones_ruta()
+
+if __name__ == "__main__":
+    main()
+
