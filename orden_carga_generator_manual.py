dd95bd045a09ab0d7a50ec4ee41ef1b1c866ed94
carloosfalco <carloosfalco@gmail.com>
2025-06-23 16:49:37 +0200

    Update orden_carga_generator_manual.py

--- orden_carga_generator_manual.py
+++ orden_carga_generator_manual.py
@@ -17,7 +17,7 @@ def formatear_fecha_con_dia(fecha):
     dia_es = DIAS_SEMANA_ES.get(dia_en, dia_en)  # Traducir al español
     return f"{dia_es} {fecha.strftime('%d/%m')}"
 
-def planificador_rutas():
+def generar_orden_carga_manual():
     st.title("📦 Generador de Orden de Carga")
     st.markdown("Completa los siguientes datos para generar una orden.")
 
