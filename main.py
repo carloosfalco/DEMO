2a7eadbab9ed0b040423516175d2aabef05c688b
carloosfalco <carloosfalco@gmail.com>
2025-06-25 12:45:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ import streamlit as st
 from rutas import planificador_rutas
 from orden_carga_generator_manual import generar_orden_carga_manual
 
-
 def main():
     st.set_page_config(page_title="Virosque TMS", page_icon="🚛", layout="wide")
 
     st.sidebar.title("📂 Menú")
+    st.sidebar.markdown("### Opciones")
+    nueva = st.sidebar.button("🧹 Nueva orden de carga")
+    if nueva:
+        st.query_params["nueva_orden"] = "1"
+        st.rerun()
+
     seleccion = st.sidebar.radio("Selecciona una opción", [
         "Planificador de rutas",
         "Orden de carga"
@@ -14,7 +19,6 @@ def main():
 
     if seleccion == "Planificador de rutas":
         planificador_rutas()
-
     elif seleccion == "Orden de carga":
         generar_orden_carga_manual()
 
