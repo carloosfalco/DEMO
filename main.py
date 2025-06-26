3628001af7305e1c661c313554131a314617e1eb
carloosfalco <carloosfalco@gmail.com>
2025-06-26 12:13:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import streamlit as st
-from rutas import planificador_rutas
-from orden_carga_generator_manual import generar_orden_carga_manual
+from gestion_remolques import gestion_remolques  # Importaremos este módulo nuevo
 
 def main():
     st.set_page_config(page_title="Virosque TMS", page_icon="🚛", layout="wide")
@@ -8,14 +6,13 @@ def main():
     st.sidebar.title("📂 Menú")
     seleccion = st.sidebar.radio("Selecciona una opción", [
         "Planificador de rutas",
-        "Orden de carga"
+        "Orden de carga",
+        "Gestión de remolques"  # Nuevo
     ])
 
     if seleccion == "Planificador de rutas":
         planificador_rutas()
-
     elif seleccion == "Orden de carga":
         generar_orden_carga_manual()
-
-if __name__ == "__main__":
-    main()
+    elif seleccion == "Gestión de remolques":
+        gestion_remolques()  # Nuevo
