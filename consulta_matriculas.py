37459259d5253c3e64c0ffde6e06b506c0297abe
carloosfalco <carloosfalco@gmail.com>
2025-07-03 10:37:54 +0200

    Update consulta_matriculas.py

--- consulta_matriculas.py
+++ consulta_matriculas.py
@@ -1,5 +1,3 @@
-import streamlit as st
-
 def consulta_matriculas():
     st.title("🤖 Instrucciones para usar el Bot de Telegram")
 
@@ -10,7 +8,8 @@ def consulta_matriculas():
     1. Abre la app de Telegram.
     2. Busca el bot por su nombre: `@virosque_consulta_bot`.
     3. Introduce la contraseña VrTc4489
-    3. Puedes escribirle en lenguaje natural cosas como:
+    4. Puedes escribirle en lenguaje natural cosas como:
         - `¿Quién lleva la 2243?`
         - `¿Quién lleva el remolque R6654BCD?`
-    4. El bot responderá a tu consulta
+    5. El bot responderá a tu consulta.
+    """)
